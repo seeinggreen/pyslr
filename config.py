557b2223ba3a2f6806741134dafd579284315d2f
Dan Green <dan@seeinggreen.co.uk>
2021-06-13 15:24:42 +0100

    Working pipeline to show render with basic head movement

--- config.py
+++ config.py
@@ -2,3 +2,6 @@ base_dir = "C:\\Users\\dan\\Documents\\HWU\\MSc\\Project";
 hg_dir = base_dir + "\\HG";
 mc_dir = base_dir + "\\monocap-master";
 pose_dict_path = mc_dir + "\\dict\\poseDict-all-K128.mat";
+blender_model_path = base_dir + "\\testMan2.blend";
+blend_exe = '"C:\\Program Files\\Blender Foundation\\Blender 2.91\\blender.exe"';
+renders_path = base_dir + "\\blenderRenders\\";
--- main.py
+++ main.py
@@ -8,6 +8,8 @@ Created on Sun May  9 14:53:40 2021
 import h5py;
 import config as cf;
 from hourglass import hg;
+from face import face;
+from blender import blender;
 import os;
 import cv2;
 import utils.utils as ut;
@@ -22,16 +24,22 @@ if __name__ == "__main__":
     #hg_model = hg.load_model(cf.hg_dir);
     
     #Get a list of images to process as frames
-    datapath = 'C:\\Users\\dan\\Documents\\HWU\\MSc\\Project\\testFrames\\';
+    #datapath = 'C:\\Users\\dan\\Documents\\HWU\\MSc\\Project\\testFrames\\';
     #datapath = 'C:\\Users\\dan\\Documents\\HWU\\MSc\\Project\\monocap\\data\\tennis\\';
-    imagelist = [x for x in os.listdir(datapath) if x.endswith(".png")];
+    datapath = 'C:\\Users\\dan\\Documents\\HWU\\MSc\\Project\\BF1n.mov'
+    #imagelist = [x for x in os.listdir(datapath) if x.endswith(".png")];
     
     #Total number of image frames
-    nImg = len(imagelist);
+    nImg = 100;
     
     #Define how to get the frame image from the iterator
-    def get_frame(path):
-        return cv2.imread(datapath + path);
+    def get_frame(cap):
+        _,frame = cap.read();
+        return frame;
+    
+    cap = cv2.VideoCapture(datapath);
+    frame0 = ut.crop(get_frame(cap),256,320);
+    cap.release();
     
     print("Getting keypoints for each frame...");
     #kpss = hg.get_kps(hg_model,iter(imagelist),nImg,get_frame);
@@ -72,37 +80,93 @@ if __name__ == "__main__":
     output = eng.PoseFromVideo('heatmap',mlhms,'dict',bDict,'th_vis',0);
     
     print("Extracting 2D and 3D pose estimates...");
-    preds_2d, preds_3d = mc.get_mc_pred(output,eng,get_frame(imagelist[0]),nImg);
+    preds_2d, preds_3d = mc.get_mc_pred(output,eng,frame0,nImg);
     
     print("Scaling 3D points...");
     ps_3d = mc.get_3d_points(preds_3d);
     
     print("Calculating plots...");
     plts = [];
-    for i in tqdm(range(100)):
-        plts.append(ut.get_3d_lines(ps_3d[i])[:,98:-78,:])
+    for i in tqdm(range(nImg)):
+        plts.append(ut.get_3d_lines(ps_3d[i])[16:-16,98:-78,:]);
+
+    print("Isolating head...");
+    tls = [];
+    brs = [];
+    for i in tqdm(range(nImg)):
+        tl,br = ut.crop_head(kpss[i][9],kpss[i][8]);
+        tls.append(tl);
+        brs.append(br);
+        
+    cap = cv2.VideoCapture(datapath);
+    frame = get_frame(cap);
+    uc = ut.get_uncrop(frame,256,320);
+    cap.set(cv2.CAP_PROP_POS_FRAMES,200);
+        
+    print("Idetifying facial features...");
+    rects,fpss,tilts = face.id_faces((get_frame(cap) for i in range(nImg)),tls,brs,uc);
+    cap.release();
+        
+    print("Preparing render...");
+    scaled_points = blender.convert_points(ps_3d);
+    blender.prepare_render(scaled_points,tilts);
     
+    print("Rendering output...");
+    blender.render();
+        
     print("Showing results...");
     cont = True;
+    cap = cv2.VideoCapture(datapath);
     
-    while cont:    
-        for i,img in enumerate(imagelist):
-            frame1 = get_frame(img);
-            frame2 = get_frame(img);
-            plot = plts[i];
+    #out = cv2.VideoWriter('..\\videoOut5.mov',cv2.VideoWriter_fourcc(*'mjpa'), 20, (768*2,768));
+    
+    while cont:
+        cap.set(cv2.CAP_PROP_POS_FRAMES,200);
+        for i in range(nImg):
+            render = cv2.imread(cf.renders_path + "{:04d}.png".format(i+1));
             
-            ut.draw_stick_figure(frame1, preds_2d[i])        
+            frame = get_frame(cap);
+            crop = ut.crop(frame,256,320);
+            frame1 = np.array(crop);
+            frame2 = np.array(crop);
+            frame3 = np.array(crop);
+            frame4 = np.array(crop);
+            plot = plts[i];
+            frame8 = np.array(crop);
+            frame9 = render[:256,293:549,:];
+            
+            ut.draw_stick_figure(frame3, preds_2d[i])        
             #For each point, highlight and label it on the original image
             for p in range(16):
                 ut.highlightPoint(frame2,[kpss[i][p][0],kpss[i][p][1]],hg.parts['mpii'][p]);
+                
+            #Draw head box
+            cv2.rectangle(frame4,tls[i],brs[i],(255,255,0));
+            
+            frame5 = ut.extract_head(frame,tls[i],brs[i],uc,256);
+            
+            frame7 = np.array(frame5);
+            frame8 = np.array(frame5);
+            cv2.rectangle(frame7, rects[i][0], rects[i][1], (0, 255, 0));
+            for p in fpss[i]:
+                cv2.circle(frame8,p,3,(255,0,0));
             
             # Display the resulting frame
-            cv2.imshow('frame',np.concatenate([frame1,frame2,plot]));
+            row1 = np.concatenate([frame1,frame2,frame3],axis=1);
+            row2 = np.concatenate([plot,frame4,frame5],axis=1);
+            row3 = np.concatenate([frame7,frame8,frame9],axis=1);
+            grid = np.concatenate([row1,row2,row3]);
             
-            cv2.waitKey(100);
+            outFrame = np.concatenate([grid,render],axis=1);
+            cv2.imshow('frame',outFrame);
+            #out.write(outFrame);
+            
+            cv2.waitKey(10);
         
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 cont = False;
                 break;
-     
+            cont = False;
     cv2.destroyAllWindows();
+    cap.release();
+    #out.release();

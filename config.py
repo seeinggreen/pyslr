4c08a843ac51d0b6342f6c6f1aba94b94349e9cb
Dan Green <dan@seeinggreen.co.uk>
2021-07-24 15:14:14 +0100

    Config clean up

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 base_dir = "C:\\Users\\dan\\Documents\\HWU\\MSc\\Project";
-hg_dir = base_dir + "\\HG";
-mc_dir = base_dir + "\\monocap-master";
+#hg_dir = base_dir + "\\HG";
+hg_dir = base_dir + "\\pytorch_stacked_hourglass";
+mc_dir = base_dir + "\\monocap";
 pose_dict_path = mc_dir + "\\dict\\poseDict-all-K128.mat";
 blender_model_path = base_dir + "\\testMan2.blend";
 blend_exe = '"C:\\Program Files\\Blender Foundation\\Blender 2.91\\blender.exe"';
 renders_path = base_dir + "\\blenderRenders\\";
-openpose_dir = base_dir + "\\openpose"
+openpose_dir = base_dir + "\\openpose";
+eval_dir = base_dir + "\\eval";
